29e1d90eeaf9d4960114a41175bca29de6ec6de3
Paulstar200 <vitalispaul48@live.com>
2021-02-07 07:52:14 +0300

    - Define Database connection

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { ApolloServer } =  require('apollo-server');
 const gql = require('graphql-tag')
+const mongoose = require('mongoose')
+
+const { MONGODB } = require('./config.js')
 
 const typeDefs = gql`
     type Query {
@@ -20,7 +23,11 @@ const server = new ApolloServer({
     resolvers
 })
 
-server.listen({ port: 5000})
+mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+    console.log('MongoDB connected')
+    return server.listen({ port: 5000 })
+})
 .then(res => {
     console.log(`Server running at ${res.url}`)
 })
